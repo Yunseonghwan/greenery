817a368b39f5c50aa309420332b32de608342ddb
Yunseonghwan <sunghwan0208@naver.com>
2021-08-03 00:23:48 +0900

    slider

--- src/pages/Home/Home.impl.tsx
+++ src/pages/Home/Home.impl.tsx
@@ -12,37 +12,38 @@ import {
   Content,
   MagzineTitle,
   UserName,
+  CardSlider,
 } from "./styles";
 
 const Home: React.FC<IHome.IProps> = () => {
   const bannerData: any = [
     {
       id: 1,
-      index: 1,
+      index: 0,
       image: TopBannerImg,
-      title: "title",
-      name: "name",
+      title: "title1",
+      name: "name1",
     },
     {
       id: 2,
-      index: 2,
+      index: 1,
       image: TopBannerImg,
-      title: "title",
-      name: "name",
+      title: "title2",
+      name: "name2",
     },
     {
       id: 3,
-      index: 3,
+      index: 2,
       image: TopBannerImg,
-      title: "title",
-      name: "name",
+      title: "title3",
+      name: "name3",
     },
     {
       id: 4,
-      index: 4,
+      index: 3,
       image: TopBannerImg,
-      title: "title",
-      name: "name",
+      title: "title4",
+      name: "name4",
     },
   ];
   const [properties, setProperties] = useState<any>(bannerData);
@@ -50,17 +51,18 @@ const Home: React.FC<IHome.IProps> = () => {
 
   const nextProperty = () => {
     const newIndex = property.index + 1;
-    console.log(newIndex);
-
+    if (property.index === properties.length - 1) {
+      return false;
+    }
     setProperty(properties[newIndex]);
   };
 
   const prevProperty = () => {
     const newIndex = property.index - 1;
+    setProperty(properties[newIndex]);
     if (property.index === 0) {
       return 0;
     }
-    setProperty(properties[newIndex]);
   };
   return (
     <MainTemplate>
@@ -70,27 +72,55 @@ const Home: React.FC<IHome.IProps> = () => {
         addText="그리너리 매거진 더 보러 가기"
       >
         <CardData>
-          <Arrow onClick={prevProperty}>
-            <span>&lt;</span>
-          </Arrow>
-          <Content>
-            {properties.map((item: any) => (
-              <CardContainer key={item.id} id={item.index}>
-                <Card src={item.image} />
-                <MagzineTitle>
-                  <span>{item.title}</span>
-                </MagzineTitle>
-                <UserName>
-                  <span>{item.name}</span>
-                </UserName>
-              </CardContainer>
-            ))}
+          <Content
+            style={
+              property.index && {
+                opacity: 1,
+                transform: "scale(1)",
+                backgroundColor: "#f3f3f3",
+                borderColor: "#f3f3f3",
+              }
+            }
+          >
+            <CardSlider
+              style={{
+                transform: `translateX(-${
+                  property.index * (100 / properties.length)
+                }%)`,
+              }}
+            >
+              {properties.map((item: any) => (
+                <CardContainer
+                  key={item.id}
+                  id={item.index}
+                  style={
+                    property.index && {
+                      opacity: 1,
+                      transform: "scale(1)",
+                      backgroundColor: "#f3f3f3",
+                      borderColor: "#f3f3f3",
+                    }
+                  }
+                >
+                  <Card src={item.image} />
+                  <MagzineTitle>
+                    <span>{item.title}</span>
+                  </MagzineTitle>
+                  <UserName>
+                    <span>{item.name}</span>
+                  </UserName>
+                </CardContainer>
+              ))}
+            </CardSlider>
           </Content>
-          <Arrow onClick={nextProperty}>
-            <span>&gt;</span>
-          </Arrow>
         </CardData>
       </ContentTemplate>
+      <Arrow onClick={nextProperty}>
+        <span>&gt;</span>
+      </Arrow>
+      <Arrow onClick={prevProperty}>
+        <span>&lt;</span>
+      </Arrow>
     </MainTemplate>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,16 +1,71 @@
 import styled from "styled-components";
 
 export const CardData = styled.div`
-  display: flex;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
   height: 609px;
-  justify-content: center;
-  align-items: center;
+  position: relative;
+  &:before &:after {
+    content: "";
+    display: block;
+    width: 50%;
+    height: 120%;
+    background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 1) 15%,
+      rgba(255, 255, 255, 0.5) 100%
+    );
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+  }
+  &:after {
+    left: auto;
+    right: 0;
+    background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 1) 15%,
+      rgba(255, 255, 255, 0.5) 100%
+    );
+  }
 `;
 
 export const Content = styled.div`
-  width: 990px;
-  overflow: hidden;
+  position: relative;
+  max-width: 226px;
+  margin: 0 auto;
+
+  &:after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 264px;
+    outline: 5px solid #61dafb;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
+export const CardSlider = styled.div`
   display: flex;
+  position: absolute;
+  transition: transform 300ms cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  .card {
+    flex: 1;
+    min-width: 200px;
+    opacity: 0.5;
+    transform: scale(0.7);
+    transition: opacity 300ms linear, border-color 300ms linear,
+      background-color 300ms linear,
+      transform 300ms cubic-bezier(0.455, 0.04, 0.515, 0.955);
+    .details {
+      opacity: 0;
+      transition: opacity 150ms linear;
+    }
+  }
 `;
 
 export const CardContainer = styled.div`
@@ -50,9 +105,4 @@ export const Arrow = styled.div`
   border: 1px solid #707070;
   border-radius: 36px;
   cursor: pointer;
-  span {
-    font-size: 22px;
-
-    color: #707070;
-  }
 `;
